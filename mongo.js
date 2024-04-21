79df5f0d7d1a9db496a47f46543947c52b7f5971
Vijay <vijaysnvpvel@gmail.com>
2024-04-21 22:28:52 +0530

    Initial commit

---
+++ mongo.js
@@ -0,0 +1,24 @@
+const mongoose=require("mongoose")
+mongoose.connect("mongodb://localhost:27017/vijay")
+.then(()=>{
+    console.log("mongodb connected");
+})
+.catch(()=>{
+    console.log('failed');
+})
+
+
+const newSchema=new mongoose.Schema({
+    email:{
+        type:String,
+        required:true
+    },
+    password:{
+        type:String,
+        required:true
+    }
+})
+
+const collection = mongoose.model("ajiths",newSchema)
+
+module.exports=collection
